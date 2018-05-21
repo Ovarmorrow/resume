89948da9e79af8adce0158af0aa003e8e8961fb8
Doodley <mpyrtle.dccc@gmail.com>
2018-05-20 20:34:02 -0400

    Changes to website
    Various tweaks and configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "app";
   navBackground: string;
   profile: string;
   pdfResume: string;
-  
-  constructor() { 
-    this.navBackground = "/resume/assets/img/navBackground.jpg";
-    this.profile = "/resume/assets/img/profile.jpeg";
-    this.pdfResume = "/resume/assets/img/pdfResume.pdf"
 
-   }
+  constructor() {
+    this.navBackground = "./assets/img/navBackground.jpg";
+    this.profile = "./assets/img/profile.jpeg";
+    this.pdfResume = "./assets/img/pdfResume.pdf";
+  }
 }
